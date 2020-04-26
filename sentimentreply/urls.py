bbc49fc72737e3216cf70dce9acb325dc94a45a6
Shivam Cholin <shivamcholin@gmail.com>
2020-04-26 22:42:31 +0530

    test

--- sentimentreply/urls.py
+++ sentimentreply/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from deploytest.views import indexhello
+from reqres.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reqres',include('reqres.urls')),
+    path('reqres',index),
+    path('hello',indexhello)
 ]

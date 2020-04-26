e9d27aff9d1af9c1296af75b06056d81f35b1555
Shivam Cholin <shivamcholin@gmail.com>
2020-04-26 22:52:00 +0530

    none

--- sentimentreply/urls.py
+++ sentimentreply/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from deploytest.views import indexhello
+from hellotest.views import indexhello
 from reqres.views import index
 
 urlpatterns = [
